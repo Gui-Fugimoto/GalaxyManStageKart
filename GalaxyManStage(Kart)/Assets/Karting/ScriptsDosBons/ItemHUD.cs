e5849210e714b194403d7fedddd0f6ab36168956
Gui-Fugimoto <guilherme.kfugimoto@gmail.com>
2021-03-24 20:01:38 -0300

    SuperCommitBros

---
+++ GalaxyManStage(Kart)/Assets/Karting/ScriptsDosBons/ItemHUD.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemHUD : MonoBehaviour
+{
+    [SerializeField] private Image customImage;
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            customImage.enabled = true;
+        }
+    }
+
+
+}
---
+++ GalaxyManStage(Kart)/Assets/Karting/ScriptsDosBons/WeaponGet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponGet : MonoBehaviour
+{
+    [SerializeField]
+    bool item = false;
+    
+    
+
+    public void ItemCollected()
+    {
+        item = true;
+    }
+}
